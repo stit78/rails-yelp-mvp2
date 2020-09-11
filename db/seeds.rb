5f0264dd89b913708fe2702979d344cc6914444d
thibaut salle <thibaut.m.salle@gmail.com>
2020-09-11 12:35:18 +0200

    yelp sans front

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+5.times do
+  restaurant = Restaurant.new
+  restaurant.name = Faker::Restaurant.name
+  restaurant.address = Faker::Address.street_address
+  restaurant.category = ["chinese", "italian", "japanese", "french", "belgian"].sample
+  restaurant.save!
+end
